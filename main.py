46008c2dce5c059f838c628b1458c4c755d04d11
maxime-antoine <maxime.antoine001@gmail.com>
2023-09-11 14:01:12 +0200

    fixed ecoscore

--- main.py
+++ main.py
@@ -188,6 +188,9 @@ with open('data/data2023.csv', 'r') as csv_file:
 
         ecoScore = round(ecoScore * 100)
 
+        if (ecoScore > 100) :
+            ecoScore = 100
+
         script += "INSERT INTO car_th (eco_score, car_transmission_type_id, car_drive_system_id, car_fuel_id, car_line_type_id, car_brand_id, model, cylinder, car_transmission, city_fuel, highway_fuel, combined_fuel, has_guzzler, gears, max_bio_fuel, annual_fuel_cost, spend_on_five_years, has_start_and_stop, fe_rating, ghg_rating, smog_rating, city_carbon, highway_carbon, combined_carbon) VALUES "
         script += "(" + str(ecoScore) + ", " + str(car['transmissionTypeId']) + ", " + str(car['driveSystemId']) + ", " + str(car['fuelTypeId']) + ", " + str(car['carLineId']) + ", " + str(car['brandId']) + ", '" + car['model'] + "', '" + car['cylinder'] + "', '" + car['transmission'] + "', '" + car['cityFuel'] + "', '" + car['highwayFuel'] + "', '" + car['combinedFuel'] + "', '" + str(car['guzzler']) + "', '" + car['gears'] + "', '" + str(car['maxBioFuel']) + "', '" + car['annualFuelCost'] + "', '" + car['spendOnFiveYears'] + "', '" + str(car['startAndStop']) + "', '" + car['fuelRate'] + "', '" + car['ghgRate'] + "', '" + car['smogRate'] + "', '" + car['cityCarbon'] + "', '" + car['highwayCarbon'] + "', '" + car['combinedCarbon'] + "');\n"
     
